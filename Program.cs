742ae646329d239523d6d49d54b949b69bb660d3
EDUARDO DE SOUSA PEREIRA LIMA <31049529812@jpmorgan.sp>
2017-10-26 21:33:07 -0200

    Pronto

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NetOffice.ExcelApi;
+
+namespace Excel_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CriarExcel();
+            LerExcel();
+        }
+
+        static void CriarExcel(){
+            Application ex = new Application();
+            ex.Workbooks.Add();
+            ex.Cells[1,1].Value = "Ford";
+            ex.ActiveWorkbook.SaveAs(@"C:\Users\31049529812\Desktop\Back_A\Project_1\Excel_Class\lista_nvo.xls");
+            ex.Quit();
+        }
+
+        static void LerExcel(){
+            Application ex = new Application();
+            ex.Workbooks.Open(@"C:\Users\31049529812\Desktop\Back_A\Project_1\Excel_Class\lista_nvo.xls");
+            string valor = ex.Cells[1,1].Value.ToString();
+            
+            Console.WriteLine(valor);
+            ex.Quit();
+        }
+    }
+}
